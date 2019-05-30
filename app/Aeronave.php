d85b741f9d70b6279c3c8b34fb293273a15fadff
Wasad <pedrorgalveias@hotmail.com>
2019-05-30 04:12:33 +0100

    Fixed Project
    Reverted merge error

--- app/Aeronave.php
+++ app/Aeronave.php
@@ -9,6 +9,12 @@ class Aeronave extends Model
     protected $primaryKey = 'matricula';
     public $timestamp = false;
     public $incrementing = false;
+
+    public function movimento()
+    {
+        return hasMany("App\Movimento", "aeronave", "matricula");
+    }
+
     protected $fillable = [
         'matricula', 'marca', 'modelo', 'num_lugares', 'conta_horas', 'preco_hora'
     ];
---
+++ resources/views/aeronaves/priceTime.blade.php
@@ -0,0 +1,35 @@
+@extends('Users.Pedro.Desktop.prj40_FINAL.prj40.resources.views.layouts.app')
+@section('content')
+
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Lista Aeronave Preço/Hora</div>
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">Matricula</th>
+                            <th scope="col">Unidade</th>
+                            <th scope="col">Minutos</th>
+                            <th scope="col">Preço</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($records as $records)
+                            <tr>
+                                <td>{{ $records->matricula }}</td>
+                                <td>{{ $records->unidade_conta_horas }}</td>
+                                <td>{{ $records->minutos }}</td>
+                                <td>{{ $records->preco }}</td>
+
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
